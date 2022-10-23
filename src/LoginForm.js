806c959909ce8db5da462d1ddb1903c54ac96fdb
Vijay Sam <vijay203.vk@gmail.com>
2022-10-23 10:11:13 +0530

    Removed bugs

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,22 +1,18 @@
 import React, { useState } from "react";
 import "./LoginForm.css";
-import user from "./user-profile.png";
 
 function LoginForm() {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const submitForm = (e) => {
     e.preventDefault();
-    for (let i = 0; i < e.target.length - 1; i++) {
-      console.log(e.target[i].value);
-    }
   };
   return (
     <div>
       <h2 className="hero1">Login</h2>
       <h3 className="hero2">Hello, Teachers!</h3>
       <p className="hero3">Enter your personal details below.</p>
-      <form>
+      <form onSubmit={submitForm}>
         <div className="inputbox">
           <div className="userinput">
             <label for={"name"}>Username:</label>
@@ -39,12 +35,12 @@ function LoginForm() {
             ></input>
           </div>
           <div className="forget">
-            <a href="#">Forget password / Username</a>
+            <a href="/">Forget password / Username</a>
           </div>
         </div>
         <input type="submit" value="Submit" className="submit-btn"></input>
         <div className="register">
-          <a href="#">Not registered? Create Account</a>
+          <a href="/">Not registered? Create Account</a>
         </div>
       </form>
     </div>
